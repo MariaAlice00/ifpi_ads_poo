69ec5faf840876720f0aa8e00fa518da785aafa3
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-12-09 21:50:30 -0300

    Create 05.java

---
+++ exercicio02/05.java
@@ -0,0 +1,10 @@
+package questao05;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        int soma = a + b;
+        System.out.println("Soma:" + soma);
+    }
+}
