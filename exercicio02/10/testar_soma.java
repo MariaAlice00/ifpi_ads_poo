3245d8be2dccffac203e9adf292d42837054ebac
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-12-09 22:42:32 -0300

    Create testar_soma.java

---
+++ exercicio02/10/testar_soma.java
@@ -0,0 +1,11 @@
+package quetao10teste;
+
+public class TestarSoma {
+    public static void main(String[] args) {
+        Soma s = new Soma();
+        s.x = 10;
+        s.y = 9;
+        
+        System.out.println(s.calculaSoma());
+    }   
+}
