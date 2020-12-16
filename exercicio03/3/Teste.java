f23c41471a72004b307faabfa6f31df1d6434f1e
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-12-15 23:43:27 -0300

    Create Teste.java

---
+++ exercicio03/3/Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+    int b = 1;
+    int a;
+
+    Teste(int a) {
+        a = b + a;
+        System.out.println(this.a);
+    }
+
+    public static void main(String[] args) {
+        Teste t = new Teste(2);
+    }
+}
