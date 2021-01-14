56827b51375d8989a9f6bd9e7e908862e14a5570
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-13 22:04:57 -0300

    Update and rename exercicio06/ProdutoPerecivel.java to exercicio06/q2/ProdutoPerecivel.java

--- exercicio06/q2/ProdutoPerecivel.java
+++ exercicio06/q2/ProdutoPerecivel.java
@@ -1,6 +1,38 @@
 public class ProdutoPerecivel extends Produto{
     String dataValidade;
 
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
     public String getDataValidade() {
         return dataValidade;
     }
