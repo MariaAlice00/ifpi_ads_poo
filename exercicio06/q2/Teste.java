394eea80c8e9694ddee8200a199d8e484a16b7aa
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-13 22:06:14 -0300

    Update Teste.java

--- exercicio06/q2/Teste.java
+++ exercicio06/q2/Teste.java
@@ -2,8 +2,16 @@ public class Teste {
     public static void main(String[] args) {
         ProdutoPerecivel produto = new ProdutoPerecivel();
 
+        produto.id = 12;
+        produto.descricao = "Um refrigerante";
+        produto.quantidade = 1;
+        produto.valor = 3.50;
         produto.setDataValidade("20/01/2021");
         
-        System.out.println(produto.dataValida("12/01/2021"));
+        System.out.println("id: " + produto.id);
+        System.out.println("Descrição: " + produto.descricao);
+        System.out.println("Quantidade: " + produto.quantidade);
+        System.out.println("Valor: R$ " + produto.valor);
+        System.out.println("Está vencido? " + produto.dataValida("22/01/2021"));
     }
 }
