d9f908f28e83bece708f17e5ef867beecc570eac
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-24 17:43:34 -0300

    Create Soma.java

---
+++ exercicio02/10/Soma.java
@@ -0,0 +1,9 @@
+/*10. Crie uma classe Soma que tenha dois atributos numéricos e que tenha um método calculaSoma(), que retorna a soma dos dois atributos.*/
+
+public class Soma {
+    int a, b;
+
+    public int calculaSoma() {
+        return a + b;
+    }
+}
