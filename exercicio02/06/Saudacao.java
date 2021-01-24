1c73e545df796afdaf74fec4e0c5577e16a20197
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-24 17:41:09 -0300

    Create Saudacao.java

---
+++ exercicio02/06/Saudacao.java
@@ -0,0 +1,7 @@
+// 6. Crie uma classe em que o método main() exiba uma mensagem de saudação usando um “println”.
+
+public class Saudacao {
+    public static void main(String[] args) {
+        System.out.println("Olá! Seja bem-vindo(a)!");
+    }
+}
