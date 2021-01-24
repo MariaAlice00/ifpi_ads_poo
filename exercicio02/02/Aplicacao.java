2f0ead57f5454dba2f3d9ed2a20fe2ef7ef366bc
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-24 17:36:55 -0300

    Create Aplicacao.java

---
+++ exercicio02/02/Aplicacao.java
@@ -0,0 +1,12 @@
+/*2. Qual o resultado da compilação do código abaixo e justifique sua resposta:*/
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        int contador;
+        System.out.println(contador++);
+    }
+}
+
+/*
+ERRO. Variável contador não foi inicializada.
+*/
