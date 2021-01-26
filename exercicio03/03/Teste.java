69046576e3c03f1acd39444233fd5af185278549
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-25 21:22:29 -0300

    Update Teste.java

--- exercicio03/03/Teste.java
+++ exercicio03/03/Teste.java
@@ -1,13 +1,16 @@
+/*Qual o resultado da execução abaixo. Justifique.*/
+
 public class Teste {
     int b = 1;
     int a;
 
     Teste(int a) {
         a = b + a;
-        System.out.println(this.a);
+        System.out.print(this.a);
     }
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Teste t = new Teste(2);
     }
 }
+
+/*O resultado é 0. O atributo t da classe Teste não foi inicializado antes, assim foi atribuído um valor automático pra ele.*/
