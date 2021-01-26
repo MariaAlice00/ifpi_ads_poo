99706ab37793df508e3d90191e79edf23f410d64
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-26 15:18:42 -0300

    Create ExecutaBanco.java

---
+++ exercicio05/01/ExecutaBanco.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+package br.ifpi.poo.ex05.cadastro;
+
+public class ExecutaBanco {
+    ....
+}
+
+/* O erro está na declaração da importação ser antes da declaração do pacote. O correto é o contrário. Primeiro declarar o pacote que a classe pertence, depois 
+declarar a importação. */
