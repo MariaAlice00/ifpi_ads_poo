cfce29a1a5c63736f64344b5a1385a5cf97ab2b7
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-25 21:34:36 -0300

    Update Valor.java

--- exercicio03/04/Valor.java
+++ exercicio03/04/Valor.java
@@ -1,8 +1,14 @@
-/* Dado o seguinte trecho de código de acumulação de um atributo valor, explique a problemática envolvida e reescreva o método com uma solução: */
+/* 
+Dado o seguinte trecho de código de acumulação de um atributo valor, explique a problemática envolvida e reescreva o método com uma solução: 
+void x(double valor) {
+    valor = valor + valor;
+}
+*/
 
+// Solução
 void x(double valor) {
     valor = valor + valor;
 }
 
-/*Há uma confusão na compreensão do código, pois atributo e parâmetro tem mesmo nome. A palavra reservada this (uma instância) permite acessar os atributos da própria  
-instância. */
+/* No código colocado pela questão, há uma confusão na compreensão do código, pois atributo e parâmetro tem mesmo nome. 
+Na solução é colocada a palavra reservada this (uma instância) que permite acessar os atributos da própria instância. */
