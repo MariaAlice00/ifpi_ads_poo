46d565bf3d800d917908c6275761069d52f58a0e
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-27 17:27:47 -0300

    Update Teste.java

--- exercicio06.2/13/Teste.java
+++ exercicio06.2/13/Teste.java
@@ -1,38 +1,11 @@
-/*(V) em uma instancia b da classe B, se fizermos b instanceof A retorna true */
-
-/*
-public class Teste {
-    public static void main(String[] args) {
-        B b = new B();
-
-        System.out.println(b instanceof A);
-    }
-}
-*/
-
-/*(V) em uma instancia a da classe A, se fizermos a instanceof B retorna false */
-
-/*
-public class Teste {
-    public static void main(String[] args) {
-        A a = new A();
-
-        System.out.println(a instanceof B);
-    }
-}
-*/
-
-/*(V) B possui o atributo1/*
-
-/*(V) Em B o atributo não é visível */
-
-/*(V) Caso o atributo fosse modificado de private para protected, poderia ser
-acessado normalmente em B */
+/* Diga o que ocorre com o código abaixo e justifique: */
 
 public class Teste {
     public static void main(String[] args) {
-        A a = new A();
+        A a = new B();
 
-        System.out.println(a instanceof B);
+        System.out.println(a.metodo2());
     }
 }
+
+/* Ocorre um erro, pois o método metodo2 é indefinido para o tipo A. Ele foi definido para B, e como o conceito de herança se aplica B herda de A, não o contrário.*/
