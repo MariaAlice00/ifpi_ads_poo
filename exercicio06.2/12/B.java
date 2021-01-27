4e0e560ae03c55c663e03a4cd791f995155a724e
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-27 17:21:06 -0300

    Create B.java

---
+++ exercicio06.2/12/B.java
@@ -0,0 +1,9 @@
+public class B extends A{
+    public String metodo1() {
+        return "metodo 1, classe B";
+    }
+
+    public String metodo2() {
+        return "metodo 2, classe B";
+    }
+}
