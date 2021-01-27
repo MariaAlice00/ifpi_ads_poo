c41f30e71927ce1d5c0142c358ecae9a4bd04210
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-01-27 17:21:25 -0300

    Create Teste.java

---
+++ exercicio06.2/12/Teste.java
@@ -0,0 +1,11 @@
+/* Qual a exibição do código abaixo: */
+
+public class Teste {
+    public static void main(String[] args) {
+        A a = new B();
+
+        System.out.println(a.metodo1());
+    }
+}
+
+/* metodo 1, classe B */
