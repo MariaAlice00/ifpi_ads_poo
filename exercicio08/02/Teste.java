afdfb4f0b79b69433fdfb4a4cea31b2e4a423709
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-04 21:59:19 -0300

    Create Teste.java

---
+++ exercicio08/02/Teste.java
@@ -0,0 +1,9 @@
+package exercicio08;
+
+public class Teste {
+    public static void main(String[] args) {
+        ClasseConcreta t = new ClasseConcreta();
+
+        System.out.println(t.somar());
+    }
+}
