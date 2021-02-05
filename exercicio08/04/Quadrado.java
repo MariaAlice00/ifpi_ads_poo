64bf11d53183551b680229f16054a3c857166cfb
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-04 22:32:55 -0300

    Create Quadrado.java

---
+++ exercicio08/04/Quadrado.java
@@ -0,0 +1,29 @@
+package exercicio08;
+
+import java.lang.Math;
+
+public class Quadrado extends FiguraGeometrica {
+    private double lado;
+
+    Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado){
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea(){
+        return Math.pow(lado, 2);
+    }
+
+    @Override
+    public double calcularPerimetro(){
+        return 4 * lado;
+    }
+}
