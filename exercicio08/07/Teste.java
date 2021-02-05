1babdc0eed632c0471eae4c1704cb323cd812a1b
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-05 20:25:42 -0300

    Create Teste.java

---
+++ exercicio08/07/Teste.java
@@ -0,0 +1,21 @@
+package exercicio08;
+
+public class Teste {
+    public static void main(String[] args) {
+        Retangulo r = new Retangulo(10, 2);   
+        Quadrado q = new Quadrado(10);
+        Circulo c = new Circulo(2);
+
+        System.out.println("RETÂNGULO");
+        System.out.println("ÁREA: " + r.calcularArea());
+        System.out.println("PERÍMETRO: " + r.calcularPerimetro());
+
+        System.out.println("\nQUADRADO");
+        System.out.println("ÁREA: " + q.calcularArea());
+        System.out.println("PERÍMETRO: " + q.calcularPerimetro());
+
+        System.out.println("\nCÍRCULO");
+        System.out.println("ÁREA: " + c.calcularArea());
+        System.out.println("PERÍMETRO: " + c.calcularPerimetro());
+    }
+}
