a77724e346b9e4ad3ebdeb05f0645c23547187e2
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-05 18:24:01 -0300

    Create Gerente.java

---
+++ exercicio08/05/Gerente.java
@@ -0,0 +1,42 @@
+package exercicio08;
+
+public class Gerente extends Funcionario {
+    private double salario;
+    private double lucrosEmpresa;
+    private double taxaParticipacao;
+
+    public Gerente(double salario, double lucrosEmpresa, double taxaParticipacao) {
+        this.salario = salario;
+        this.lucrosEmpresa = lucrosEmpresa;
+        this.taxaParticipacao = taxaParticipacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getLucrosEmpresa() {
+        return lucrosEmpresa;
+    }
+
+    public void setLucrosEmpresa(double lucrosEmpresa) {
+        this.lucrosEmpresa = lucrosEmpresa;
+    }
+
+    public double getTaxaParticipacao(){
+        return taxaParticipacao;
+    }
+
+    public void setTaxaParticipacao(double taxaParticipacao) {
+        this.taxaParticipacao = taxaParticipacao;
+    }
+
+    @Override
+    public double getBonificacao() {
+        return salario + ((taxaParticipacao / 100) * lucrosEmpresa);
+    }
+}
