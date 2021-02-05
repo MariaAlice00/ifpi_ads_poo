29f568839cc1604d9dcdc47fc19aa354881c7cb7
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-05 20:54:18 -0300

    Create Circulo.java

---
+++ exercicio08/08.09/Circulo.java
@@ -0,0 +1,41 @@
+package exercicio0809;
+import java.lang.Math;
+
+public class Circulo implements FiguraGeometrica, Comparavel {
+    private double raio;
+
+    Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio){
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea(){
+        return 3.14 * Math.pow(raio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro(){
+        return 2 * 3.14 * raio;
+    }
+
+    @Override
+    public int comparar(FiguraGeometrica fg) {
+        if (fg.calcularArea() > this.calcularArea()) {
+            return -1;
+        }
+        else if (fg.calcularArea() == this.calcularArea()) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+}
