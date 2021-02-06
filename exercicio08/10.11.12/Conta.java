de9a2ddbaf8d65567d10a6f91596e43b93723d8d
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2021-02-05 21:25:26 -0300

    Create Conta.java

---
+++ exercicio08/10.11.12/Conta.java
@@ -0,0 +1,22 @@
+package exercicio08;
+
+public class Conta{
+    private String nome;
+    private double saldo;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+}
